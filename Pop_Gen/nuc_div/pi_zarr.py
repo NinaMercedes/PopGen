3ba308837faff3e63b309dcb1cc872bd9fb53900
Nina <78304174+NinaMercedes@users.noreply.github.com>
2025-01-22 13:40:16 +0000

    Add window stat

--- Pop_Gen/nuc_div/pi_zarr.py
+++ Pop_Gen/nuc_div/pi_zarr.py
@@ -34,12 +34,37 @@ def nucleotide_diversity(zarrfile, metadata, out_dir, population, wgs_id):
     pi = allel.mean_pairwise_difference(acu)
     pi_df = pd.DataFrame({'pi':pi})
     f_name = out_dir + "/" + pop + "_pi.csv"
-    pi_df.to_csv(f_name)   
+    pi_df.to_csv(f_name)  
+
+def window_diversity(zarrfile, metadata, out_dir, population, wgs_id):
+  callset = zarr.open(zarrfile, mode='r')
+  sample_names = callset['samples'][:]
+  pos_index = allel.ChromPosIndex(callset['variants/CHROM'][:], callset['variants/POS'][:])
+  genotype_all = allel.GenotypeChunkedArray(callset['calldata']['GT'])
+  df_samples = pd.read_csv(metadata, sep="\t")
+  df_samples['domain'] = df_samples[wgs_id]
+  df_samples =pd.DataFrame({'domain':sample_names}).merge(df_samples, on='domain', how='left')
+  geo_r = df_samples[population].unique() 
+  geo_r  = [x for x in geo_r if str(x) != 'nan'] 
+  #geo_r = ["Indonesia", "Eritrea", "Brazil", "Ecuador", "French_Guiana", "Guyana", "Zambia", "Burkina_Faso", "Guinea_Bissau", "ivory_coast"] 
+  for pop in geo_r: #check combs vs permutations
+    print(pop)
+    subpops = {
+    pop: df_samples[df_samples[population] == pop].index
+    }
+    # allele counts
+    acs = genotype_all.count_alleles_subpops(subpops)
+    acu = allel.AlleleCountsArray(acs[pop])
+    pos = np.arange(1,acu.shape[0]+1)
+    pi, windows, n_bases, counts = allel.windowed_diversity(pos, acu, size=25000)
+    pi_df = pd.DataFrame({'pi':pi})
+    f_name = out_dir + "/" + pop + "_pi_window.csv"
+    pi_df.to_csv(f_name)    
 
 
 def main(args):
-  nucleotide_diversity(args.zarr, args.metadata, args.out_dir, args.population, args.wgs_id)
-
+  window_diversity(args.zarr, args.metadata, args.out_dir, args.population, args.wgs_id)
+  
 parser = argparse.ArgumentParser(description='Pairwise diversity statistics analysis',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--metadata',help='tsv file containing metadata',required=True)
 parser.add_argument('--zarr',help='zarr file',required=True)
